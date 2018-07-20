a7742bd583556798c5d864fb52cccf085ee1c7b8
Peacess <peacess@163.com>
2018-07-20 16:24:23 +0800

    add isnil

---
+++ sutils/skit/skit.go
@@ -0,0 +1,37 @@
+package skit
+
+import (
+	"reflect"
+)
+
+func IsNil(any interface{}) bool {
+	re := false
+	if any != nil {
+		v := reflect.ValueOf(any)
+
+		if v.Kind() == reflect.Ptr || v.Kind() == reflect.Interface {
+			re = v.IsNil()
+			if !re {
+				for {
+					v2 := v.Elem()
+					if v2.Kind() != reflect.Ptr && v2.Kind() != reflect.Interface {
+						break
+					}
+					re = v2.IsNil()
+					if re {
+						break
+					}
+					v = v2
+				}
+			}
+
+		}
+	}
+	return re
+}
+
+func RealType(t interface{}) (ty reflect.Type) {
+	ty = nil
+	//tt := reflect.TypeOf(t)
+	return
+}
