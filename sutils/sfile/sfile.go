725a558063a80667aebdb9885537900991f97cfe
Peacess <peace@163.com>
2019-11-27 09:03:51 +0800

    add isFile, isDir fun

--- sutils/sfile/sfile.go
+++ sutils/sfile/sfile.go
@@ -12,3 +12,17 @@ func ExistFile(file string) bool {
 	_, err := os.Stat(file)
 	return (err == nil || os.IsExist(err))
 }
+
+//如果存在且是文件，返回true
+//注： !IsDir  !=  IsFile 因为还有出错的情况， 所以写了两个函数
+func IsFile(str string) bool {
+	fileInfo, err := os.Stat(str)
+	return err == nil && !fileInfo.IsDir()
+}
+
+//如果存在且是目录，返回true
+//注： !IsDir  !=  IsFile 因为还有出错的情况， 所以写了两个函数
+func IsDir(str string) bool {
+	fileInfo, err := os.Stat(str)
+	return err == nil && fileInfo.IsDir()
+}
